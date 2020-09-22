7e107fcc5297fdc9e480bd824fd3a98fa714217f
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-22 15:41:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 
 }
 
@@ -14,3 +17,8 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+// fetch('https://anapioficeandfire.com/api/books')
+//   .then(resp => resp.json())
+//   .then(json => console.log(renderBooks));
